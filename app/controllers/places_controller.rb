f4c91a8c15168d6b69acd3e9f9169157efcfd415
Tyna Huynh <tynahuynh@gmail.com>
2017-02-14 05:36:23 +0000

    connected the submit/new page to the database. and redirects them to root page after submitting.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,11 +1,22 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all.paginate(page: params[:page], per_page: 2)
+    @places = Place.all.paginate(page: params[:page], per_page: 10)
   end
 
   def new
     @place = Place.new
   end
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end  
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end    
 
 end
